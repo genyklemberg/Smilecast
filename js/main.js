2e4d5459720614da277820e6b73e6e39a6c2632b
genyklemberg <aputop@yahoo.com>
2015-07-15 22:25:34 +0000

    Added pic and CV

---
+++ js/main.js
@@ -0,0 +1,11 @@
+'use strict';
+function showthis() {
+    alert("I'm golden bridge");
+}
+var myNum=123,
+    myString="some string",
+    myBool=true,
+    myNull=null,
+    MyUndef=undefined;
+
+console.log(myNum);
